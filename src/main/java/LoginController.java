5d35a1c1c0229af9a741c28ba01358b6c1700925
emival <zordaso@gmail.com>
2019-02-14 12:01:45 +0100

    Implemented login feature.

--- src/main/java/LoginController.java
+++ src/main/java/LoginController.java
@@ -1,10 +1,18 @@
+import ch.simas.jtoggl.JToggl;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
 
     @FXML
     private Button SubmitBtn;
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField PasswordField;
+    private static JToggl jToggl;
 
     public void initialize() {
         setKeyAndClickListeners();
@@ -16,6 +24,11 @@ public class LoginController {
 
     private void login() {
         //TODO: verify login credentials
+        String user = emailField.getText();
+        String password = PasswordField.getText();
+        jToggl = new JToggl(user, password);
+        jToggl.switchLoggingOn();
+
         try {
             new GUIBaseController().start();
         } catch (Exception e) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import ch.simas.jtoggl.JToggl;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
