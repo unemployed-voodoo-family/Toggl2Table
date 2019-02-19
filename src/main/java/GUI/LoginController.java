f0a2f0399c24c637e831f5b6d1d3273480d0103f
Big Black Mama <bendikno@gmail.com>
2019-02-19 13:10:36 +0100

    added method to login by pressing the enter button

--- src/main/java/GUI/LoginController.java
+++ src/main/java/GUI/LoginController.java
@@ -6,17 +6,18 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.io.IOException;
 
 public class LoginController {
 
     @FXML
-    private Button SubmitBtn;
+    private Button submitBtn;
     @FXML
     private TextField emailField;
     @FXML
-    private PasswordField PasswordField;
+    private PasswordField passwordField;
     private JToggl jToggl;
 
 
@@ -25,38 +26,45 @@ public class LoginController {
     }
 
     private void setKeyAndClickListeners() {
-        SubmitBtn.setOnAction(event -> login());
+        submitBtn.setOnAction(event -> login());
     }
 
     private void login() {
         //TODO: verify login credentials
         String user = emailField.getText();
-        String password = PasswordField.getText();
+        String password = passwordField.getText();
         jToggl = new JToggl(user, password);
         jToggl.switchLoggingOn();
-        if(isLoggedIn()){
+        if(isLoggedIn()) {
             try {
                 new GUIBaseController().start();
-            } catch (IOException e) {
+            }
+            catch(IOException e) {
                 e.printStackTrace();
             }
         }
 
 
-
     }
 
-    private boolean isLoggedIn(){
+    private boolean isLoggedIn() {
         boolean loggedIn;
         User user = jToggl.getCurrentUser();
         String userString = user.toString();
-        if(userString.contains("api_token")){
+        if(userString.contains("api_token")) {
             loggedIn = true;
         }
-        else{
+        else {
             loggedIn = false;
         }
         return loggedIn;
+    }
+
+    @FXML
+    public void enterButtonPressed(KeyEvent e) {
+        if(e.getCode().toString().equals("ENTER")) {
+            login();
+        }
     }
 
 }
