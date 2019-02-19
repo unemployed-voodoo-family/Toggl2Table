d7fbddef915c712c5974520125e6bc25b8bdde28
Lars Ous <larsoyvind28@gmail.com>
2019-02-19 16:52:57 +0100

    Implemented specify work hours (still WIP)

--- src/main/java/GUI/Content/SettingsController.java
+++ src/main/java/GUI/Content/SettingsController.java
@@ -1,15 +1,24 @@
 package GUI.Content;
 
+import GUI.DateRange;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Skin;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 
-import java.io.IOException;
+import java.io.*;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 
 public class SettingsController {
 
@@ -31,6 +40,8 @@ public class SettingsController {
     private Button logoutBtn;
     @FXML
     private Pane contentRoot;
+    @FXML
+    private TextField hoursField;
 
 
     public Node loadFXML() throws IOException {
@@ -38,5 +49,90 @@ public class SettingsController {
         return FXMLLoader.load(r);
     }
 
+    public void initialize() {
+        setKeyAndClickListeners();
 
+    }
+
+    private void setKeyAndClickListeners() {
+
+        confirmHoursBtn.setOnAction(event -> {
+            try {
+                setWorkHours();
+            }
+            catch(URISyntaxException e) {
+                e.printStackTrace();
+            }
+            catch(IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void setWorkHours() throws URISyntaxException, IOException {
+        //TODO: feedback
+        //TODO: make hoursField only accept numbers
+        //get saved work hours file and add to arraylist
+        URL resourceUrl = getClass().getResource("/Settings/settings.properties");
+        File filename = new File(resourceUrl.toURI().getPath());
+        Properties props = new Properties();
+        props.load(new FileInputStream(filename));
+
+        //get values from datepicker
+        LocalDate fromDate = hoursFromField.getValue();
+        LocalDate toDate = hoursToField.getValue();
+        Double hours = Double.parseDouble(hoursField.getText());
+        if(fromDate != null && toDate != null && hours != null) {
+            DateRange range = new DateRange(fromDate, toDate);
+            fixHoursOverlap(props, range, hours);
+            props.store(new FileOutputStream(filename), "Datez");
+        }
+        else {
+            System.out.println("null");
+        }
+    }
+
+    private void fixHoursOverlap(Properties props, DateRange newRange, Double newValue) {
+
+        Set<String> hours = props.stringPropertyNames();
+        if(! hours.isEmpty()) {
+            Iterator it = hours.iterator();
+            while(it.hasNext()) {
+                String key = (String) it.next();
+                String value = props.getProperty(key);
+                boolean changed = false;
+                String[] dates = key.split(" - ");
+                System.out.println();
+                DateRange oldRange = new DateRange(LocalDate.parse(dates[0]), LocalDate.parse(dates[1]));
+
+                //if new "from" value overrides old "to" value
+                if(newRange.getFrom().isAfter(oldRange.getTo())) {
+                    oldRange.setTo(newRange.getFrom().minusDays(1));
+                    changed = true;
+                }
+                //if new "to" value overrides old "from" value
+                if(newRange.getTo().isAfter(oldRange.getFrom())) {
+                    oldRange.setFrom(newRange.getTo().plusDays(1));
+                    changed = true;
+                }
+                //if values are illogical, remove
+                if(oldRange.getFrom().equals(oldRange.getTo()) || oldRange.getFrom().isAfter(oldRange.getTo())) {
+                    props.remove(key);
+                }
+                if(changed) {
+                    props.remove(key);
+                    props.put(oldRange.getFrom().toString() + " - " + oldRange.getTo().toString(), value);
+                }
+                props.put(newRange.getFrom().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + " - "
+                                  + newRange.getTo().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")), newValue.toString());
+                System.out.println(props.stringPropertyNames().toString());
+            }
+        }
+        else {
+            props.put(newRange.getFrom().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")) + " - "
+                              + newRange.getTo().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")), newValue.toString());
+
+        }
+    }
 }
+
--- src/main/java/GUI/DateRange.java
+++ src/main/java/GUI/DateRange.java
@@ -1,3 +1,29 @@
 package GUI;
 
-public class DateRange {}
+import java.time.LocalDate;
+
+public class DateRange {
+    private LocalDate from;
+    private LocalDate to;
+
+    public DateRange(LocalDate from, LocalDate to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public LocalDate getFrom() {
+        return from;
+    }
+
+    public LocalDate getTo() {
+        return to;
+    }
+
+    public void setFrom(LocalDate from) {
+        this.from = from;
+    }
+
+    public void setTo(LocalDate to) {
+        this.to = to;
+    }
+}
