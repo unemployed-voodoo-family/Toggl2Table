8be8d1dd3381d1db9631daa3a70888e193391ad0
Mairesu <marusnak@online.no>
2019-02-28 16:22:47 +0100

    manual merge of LoginLogic

--- src/main/java/UnemployedVoodooFamily/Logic/LoginLogic.java
+++ src/main/java/UnemployedVoodooFamily/Logic/LoginLogic.java
@@ -20,42 +20,24 @@ public class LoginLogic {
         boolean loggedIn = false;
         try {
             session.setSession(jToggl);
-            Platform.runLater(() -> {
-                try {
-                    new GUIBaseController().start();
-                }
-                catch(IOException ioe)  {
-                    System.out.println(ioe.getMessage());
-                }
+            Thread togglThread = new Thread(() -> {
+                Platform.runLater(() -> {
+                    try {
+                        new GUIBaseController().start();
+                    }
+                    catch(IOException ioe) {
+                        System.out.println(ioe.getMessage());
+                    }
+                });
             });
+            togglThread.start();
             loggedIn = true;
         }
         catch(RuntimeException e) {
             //Forbidden!
             Session.getInstance().terminateSession();
         }
+
         return loggedIn;
     }
-
-
-    /*private boolean isLoggedIn() {
-        String userString;
-        boolean loggedIn = false;
-        try{
-            User user = jToggl.getCurrentUser();
-            userString = user.toString();
-            if(userString.contains("api_token")) {
-                loggedIn = true;
-            }
-            else {
-                loggedIn = false;
-                System.out.println("\nWrong username or password");
-            }
-        }
-        catch(RuntimeException t){
-            System.out.println(t.getMessage());
-        }
-        return loggedIn;
-    }*/
-
 }
