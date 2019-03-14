5a6d5a0adc91fc7189a310d61dc8b467bc6ac86a
Lars Ous <larsoyvind28@gmail.com>
2019-03-14 10:59:53 +0100

    cleanup

--- src/main/java/UnemployedVoodooFamily/Logger.java
+++ src/main/java/UnemployedVoodooFamily/Logger.java
@@ -21,29 +21,11 @@ public class Logger {
     private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm");
 
     private Logger() {
-        /*File theDir = new File(FilePath.LOGS_HOME.getPath());
-
-        // if the directory does not exist, create it
-        if (!theDir.exists()) {
-            System.out.println("creating directory: " + theDir.getName());
-            boolean result = false;
-
-            try{
-                theDir.mkdir();
-                result = true;
-            }
-            catch(SecurityException se){
-                //handle it
-            }
-            if(result) {
-                System.out.println("DIR created");
-            }
-        }*/
     }
 
     public void dumpCollection(Collection<?> dataset, String name) {
-        File file = null;
-        file = new File(FilePath.LOGS_HOME.getPath() + File.separator + LocalDateTime.now().format(formatter) + "." + name + "-dump.json");
+        File file = new File(FilePath.LOGS_HOME.getPath() + File.separator + LocalDateTime.now()
+                                                                                          .format(formatter) + "." + name + "-dump.json");
         try {
             file.getParentFile().mkdirs();
             file.createNewFile();
@@ -65,12 +47,6 @@ public class Logger {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.setPrettyPrinting().create();
         gson.toJson(dataset, writer);
-        /*JsonArray jsonArray= new JsonParser().parse(gson.toJson(dataset)).getAsJsonArray();
-        String jsonStr = jsonArray.get(0).toString();
-        TimeEntry t = new TimeEntry(jsonStr);
-        System.out.println(t.toString());
-        //TimeEntry t = new TimeEntry();
-        Convert json to timeentry (for later reference)*/
         try {
             writer.close();
         }
