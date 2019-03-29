cafa98c4fbf0c931b9ff3df448015f7c4bd22fdd
emival08 <zordaso@gmail.com>
2019-03-29 09:23:35 +0100

    Adds the beginning of GUI testing

--- src/main/java/UnemployedVoodooFamily/Tests/LoginLogicTest.java
+++ src/main/java/UnemployedVoodooFamily/Tests/LoginLogicTest.java
@@ -1,16 +1,29 @@
 package UnemployedVoodooFamily.Tests;
 
+import UnemployedVoodooFamily.Logic.LoginLogic;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 import static org.junit.Assert.*;
 
-public class LoginLogicTest {
+public class LoginLogicTest extends LoginLogic {
 
-    private boolean loggedIn = true;
+    private LoginLogic loginLogic = new LoginLogic();
 
     @Test
-    public void attemptAuthentication() {
-        boolean loginResult = true;
-        assertEquals(loggedIn, loginResult);
+    public void successfulAttemptAuthentication() {
+        //Not yet implemented
+    }
+
+    @Test
+    public void failedAttemptAuthentication() {
+        boolean testResult = loginLogic.attemptAuthentication("Lars", "password", false, false);
+        assertFalse(testResult);
+    }
+
+    @Test
+    public void successfulSaveUsernameAndPassword() {
+        //Not yet implemented
     }
 }
