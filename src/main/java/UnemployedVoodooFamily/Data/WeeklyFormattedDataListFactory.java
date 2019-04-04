ee67764e0d48e9398bcf53f7ee7e738b1c5a60b9
Mairesu <marusnak@online.no>
2019-04-04 15:01:12 +0200

    better checking for ongoing timers

--- src/main/java/UnemployedVoodooFamily/Data/WeeklyFormattedDataListFactory.java
+++ src/main/java/UnemployedVoodooFamily/Data/WeeklyFormattedDataListFactory.java
@@ -6,96 +6,47 @@ import ch.simas.jtoggl.TimeEntry;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.IsoFields;
-import java.time.temporal.TemporalAdjuster;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
 public class WeeklyFormattedDataListFactory {
-    /*
-    private DayOfWeek weekDay;
-    private Double workedHours;
-    private Double supposedHours;
-    private Double overtime;
-    private LocalDate day;
-    private String note = "oppsie woopsie";
-    private PropertiesLogic propsLogic = new PropertiesLogic();
-
-    private List<TimeEntry> dailyTimeEntries = new ArrayList<>();
-    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-
-    private Properties workHours = Session.getInstance().getWorkHours();
-
-     */
 
     private ArrayList<DailyFormattedDataModel> weeklyList;
 
     public List<DailyFormattedDataModel> buildWeeklyDataList(List<TimeEntry> timeEntries, int weekNumber, int year)   {
         weeklyList = new ArrayList<>();
-        setupWeeklyList(weekNumber, year);
-        populateWeeklyList();
-        int weeklyEntries = 0;
-        return weeklyList;
-    }
 
-    private void setupWeeklyList(int weekNumber, int year)    {
+        //finds the first day of the selected week
         LocalDate weeksFirstDate = LocalDate.ofYearDay(year, 50)
                                             .with(IsoFields.WEEK_OF_WEEK_BASED_YEAR, weekNumber)
                                             .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
 
+        //Creates a list with the entire week
         for(DayOfWeek weekday : DayOfWeek.values()) {
+
+            //Create current date to process
             LocalDate date = weeksFirstDate.plusDays(weekday.getValue()-1);
-            weeklyList.add(new DailyFormattedDataModel(0.00,0.00, date, ""));
-        }
-    }
+            double workedHours = 0.00;
 
-    private void populateWeeklyList()   {
-
-    }
-
-    /*
-    public WeeklyFormattedDataListFactory(LocalDate day) {
-        this.day = day;
-    }
-
-    public WeeklyFormattedDataListFactory addTimeEntry(TimeEntry timeEntry) {
-        if(timeEntry != null) {
-            LocalDate startDate = timeEntry.getStart().toLocalDate();
-            if(startDate != null && startDate.equals(day)) {
-                dailyTimeEntries.add(timeEntry);
+            if(!timeEntries.isEmpty())  {
+                for(TimeEntry t : timeEntries)  {
+                    //Check if there is a timer running and ignore it if it is
+                    if(null != t.getStop()) {
+                        if(t.getStart().toLocalDate().isEqual(date) && t.getStop().toLocalDate().isEqual(date)) {
+                            workedHours += ((double)t.getDuration() % 86400) / 3600;
+                        }
+                    }
+                }
             }
+                //TODO get correct supposed work hours
+            weeklyList.add(new DailyFormattedDataModel(workedHours,7.5, date, ""));
         }
-        return this;
-    }
 
-    public DailyFormattedDataModel build() {
-        Double workedSeconds = sumDurations();
-        this.weekDay = this.day.getDayOfWeek();
-        this.supposedHours = findSupposedHours();
-        this.workedHours = (workedSeconds % 86400) / 3600;
-        return new DailyFormattedDataModel(this.workedHours, this.supposedHours, this.day, this.note);
+        return weeklyList;
     }
-
-    private Double findSupposedHours() {
-        this.supposedHours = 0.0;
-        for(String key: workHours.stringPropertyNames()) {
-            if(DateRange.ofString(key, formatter).contains(this.day)) {
-                return Double.parseDouble(workHours.getProperty(key));
-            }
-        }
-        return 7.5; //TODO: get default value from somewhere
-    }
-
-    private Double sumDurations() {
-        double workedSeconds = 0.0;
-        for(TimeEntry t: dailyTimeEntries) {
-            workedSeconds += t.getDuration();
-        }
-        return workedSeconds;
-    }
-
-     */
 }
