bf235b60708c279cb83151921a67c79980403a02
Mairesu <marusnak@online.no>
2019-04-11 17:30:25 +0200

    Created accumulated work hours in excel sheet

--- src/main/java/UnemployedVoodooFamily/Logic/ExcelExportHandler.java
+++ src/main/java/UnemployedVoodooFamily/Logic/ExcelExportHandler.java
@@ -5,8 +5,6 @@ import ch.simas.jtoggl.TimeEntry;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.time.Month;
 import java.util.HashMap;
 import java.util.List;
@@ -14,11 +12,14 @@ import java.util.List;
 //This class should gather all the data necessary and write them to the excel document
 public class ExcelExportHandler {
 
-    private static ExcelWriter excelWriter;
-    private static HashMap<String, List> monthlyDataLists;
+    private ExcelWriter excelWriter;
+    private HashMap<String, List> monthlyDataLists;
+    private int year;
+
 
     public ExcelExportHandler(List<TimeEntry> timeEntries, int year) {
         excelWriter = new ExcelWriter();
+        this.year = year;
         monthlyDataLists = new HashMap<>();
         for(Month month: Month.values()) {
             monthlyDataLists.put(StringUtils.capitalize(month.toString().toLowerCase()),
@@ -28,7 +29,7 @@ public class ExcelExportHandler {
 
     public boolean makeExcelDocument() throws IOException {
         //generateDummyLists(); //Remove this later, dummy
-        boolean exportSuccess = excelWriter.generateExcelSheet(monthlyDataLists);
+        boolean exportSuccess = excelWriter.generateExcelSheet(monthlyDataLists, year);
         if(exportSuccess)   {
             System.out.println("Successfully exported to excel document");
         }
